89f99b8f4764f8780481766c748ce57315108fb3
Fidel Cruz Reyes <fave6@Laptop-HP-Fidel>
2025-11-25 15:27:48 -0600

    feat: agregar login

--- src/gestortutoriasfx/GestorTutoriasFX.java
+++ src/gestortutoriasfx/GestorTutoriasFX.java
@@ -4,9 +4,12 @@
  */
 package gestortutoriasfx;
 
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
@@ -20,24 +23,17 @@ public class GestorTutoriasFX extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
+        try {
+            Parent vista  = FXMLLoader.load(getClass().getResource("vista/FXMLInicioSesion.fxml"));
+            Scene escenaInicioSesion = new Scene(vista);
             
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+            primaryStage.setScene(escenaInicioSesion);
+            primaryStage.setTitle("Inicio de sesión");
+            primaryStage.show();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            System.out.println("Error: " + ex.getMessage());
+        }
     }
 
     /**
