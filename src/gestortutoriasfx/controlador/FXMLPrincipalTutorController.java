aadff40ddd02bab0ef365d59d06ddde571b8d584
Fidel Cruz Reyes <fave6@Laptop-HP-Fidel>
2025-11-26 00:35:30 -0600

    feat: añadir (vistas/controladores) principales de los usuarios

---
+++ src/gestortutoriasfx/controlador/FXMLPrincipalTutorController.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package gestortutoriasfx.controlador;
+
+import gestortutoriasfx.interfaz.IPrincipalControlador;
+import gestortutoriasfx.modelo.pojo.Usuario;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author fave6
+ */
+public class FXMLPrincipalTutorController implements Initializable, IPrincipalControlador{
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @Override
+    public void inicializarInformacion(Usuario usuario) {
+        System.out.println("Bienvenido Tutor: " + usuario.getUsuario());
+    }
+    
+}
