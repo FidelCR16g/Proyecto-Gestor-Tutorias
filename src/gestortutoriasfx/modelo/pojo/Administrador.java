5b71639346ba5f2273fb3bbc9290cd21a45b67a7
Fidel Cruz Reyes <fave6@Laptop-HP-Fidel>
2025-11-27 01:55:08 -0600

    docs: Actualización de encabezados de clase
    - Se agregan comentarios de autoría y descripción en POJOs de Administrador y Coordinador conforme al estándar del proyecto.

--- src/gestortutoriasfx/modelo/pojo/Administrador.java
+++ src/gestortutoriasfx/modelo/pojo/Administrador.java
@@ -5,9 +5,20 @@
 package gestortutoriasfx.modelo.pojo;
 
 /**
+ * Nombre de la Clase: Administrador
  *
- * @author fave6
+ * Proyecto: Sistema de Gestión de Tutorías (SGT)
+ *
+ * Institución: Universidad Veracruzana
+ * Curso: Principios de Construcción de Software
+ *
+ * Autor: Fidel Cruz Reyes
+ * Fecha de Creación: 25/11/2025
+ *
+ * Descripción:
+ * Clase encargada de contener los datos del administrador.
  */
+
 public class Administrador {
     
 }
